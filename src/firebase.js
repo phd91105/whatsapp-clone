06cb9a6e2e2049ef43c2ae79d44fc7df9d75e2ea
Phạm Duy <PHD051199@gmail.com>
2021-02-09 19:19:07 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB13PntUEDMkFbTt2psiHOZtQih83qrf88",
-//     authDomain: "whatsapp-clone-fafc9.firebaseapp.com",
-//     databaseURL: "https://whatsapp-clone-fafc9.firebaseio.com",
-//     projectId: "whatsapp-clone-fafc9",
-//     storageBucket: "whatsapp-clone-fafc9.appspot.com",
-//     messagingSenderId: "1027981883218",
-//     appId: "1:1027981883218:web:f62f32ffe8e5701f0f2c8b",
-//     measurementId: "G-0YMF5ZZL8N"
-//   };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD3c-g9buyeJnYZ-6_1NDZ8TQz1n4fhKn4",
-  authDomain: "whatsapp-73989.firebaseapp.com",
-  databaseURL: "https://whatsapp-73989.firebaseio.com",
-  projectId: "whatsapp-73989",
-  storageBucket: "whatsapp-73989.appspot.com",
-  messagingSenderId: "166827944927",
-  appId: "1:166827944927:web:28451dca14499bc919326a",
-  measurementId: "G-E7CXYXDVYG",
+  apiKey: "AIzaSyB4Zp4QIjHU0aqCRhyIqz8iWDyE4HAiYOo",
+  authDomain: "whatsapp-271c0.firebaseapp.com",
+  databaseURL: "https://whatsapp-271c0-default-rtdb.firebaseio.com",
+  projectId: "whatsapp-271c0",
+  storageBucket: "whatsapp-271c0.appspot.com",
+  messagingSenderId: "998262847070",
+  appId: "1:998262847070:web:4d02b12c03f72077a666cf",
+  measurementId: "G-3TPMRJ05KD"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
